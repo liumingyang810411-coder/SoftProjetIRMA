7a44f177868d58a75336984900f61d44630ec059
Mingyang Liu <liumingyang810411@gmail.com>
2026-01-21 21:17:42 +0100

    final version

--- SoftProjetIRMA.ino
+++ SoftProjetIRMA.ino
@@ -59,7 +59,7 @@ void Init_Btn(){
   pinMode(Shutter_Led, OUTPUT);
 }
 bool btn_pressed(int btn){
-  return (digitalRead(btn)==LOW);
+  return (digitalRead(btn)== LOW);
 }
 
 /* =========================================================
@@ -67,7 +67,7 @@ bool btn_pressed(int btn){
  ========================================================= */
 static Servo shutterServo;
 static const int SERVO_OPEN_DEG = 30;
-static const int SERVO_CLOSE_DEG  = 112;
+static const int SERVO_CLOSE_DEG  = 111;
 void servo_init() {
   shutterServo.attach(SERVO_PIN);
   servo_set_deg(SERVO_CLOSE_DEG);
@@ -129,22 +129,31 @@ void stepper_one_step(StepDir dir, uint8_t pwm, uint16_t dt_ms){
   }
   delay(dt_ms);
 }
+int counter_pas =0;
 void stepper_motor_control_pas(StepDir dir, uint8_t pwm, uint16_t dt_ms,int pas){
   if(dir==STEP_FWD){
     for (int i = 0; i < pas; i++){
       if(is_limit_capFWD_pressed()==0){
         stepper_one_step(dir, pwm, dt_ms);
         //delay(3000);
+        counter_pas-=1;
        }else{
         stepper_stop();
-        break;}}
+        break;
+      }       
+        print();
+ }
   }else{
     for (int i = 0; i < pas; i++){
       if(is_limit_capREV_pressed()==0){
         stepper_one_step(dir, pwm, dt_ms);
+        counter_pas+=1;
        }else{
         stepper_stop();
-        break;}}
+        break;
+        }       
+        print();
+     }
   }
   stepper_stop();
 }
@@ -154,21 +163,13 @@ void stepper_motor_control_pas(StepDir dir, uint8_t pwm, uint16_t dt_ms,int pas)
 static const uint8_t PWM = 200;
 static const uint8_t vitesse = 30;
 static const uint8_t big_step = 17;
-int counter_pas =0;
+
 float position_act=0;
 
-bool pressed_edge(uint8_t pin, unsigned long cooldown_ms = 300) {
+bool pressed_edge(uint8_t pin) {
   static uint8_t last_state[30];
-  static unsigned long last_time[30];
   uint8_t cur = digitalRead(pin);
-  unsigned long now = millis();
-  bool edge = false;
-  if (last_state[pin] == HIGH && cur == LOW) {
-    if (now - last_time[pin] >= cooldown_ms) {
-      edge = true;
-      last_time[pin] = now; 
-    }
-  }
+  bool edge = (last_state[pin] == HIGH && cur == LOW);
   last_state[pin] = cur;
   return edge;
 }
@@ -176,7 +177,7 @@ void initialisation_position(){
   stepper_motor_control_pas(STEP_REV,200,50,340);
 }
 void reset_position(){
-  if(pressed_edge(Btn_Big_FWD) & pressed_edge(Btn_Big_REV)){
+  if(btn_pressed(Btn_Big_FWD) & btn_pressed(Btn_Big_REV)){
     initialisation_position();
     counter_pas=0;
     position_act=0;
@@ -184,32 +185,46 @@ void reset_position(){
 }
 bool only_one_button_pressed() {
   int count = 0;
-  if (digitalRead(Btn_Small_FWD) == LOW) count++;
-  if (digitalRead(Btn_Small_REV) == LOW) count++;
-  if (digitalRead(Btn_Big_FWD)   == LOW) count++;
-  if (digitalRead(Btn_Big_REV)   == LOW) count++;
+  if (digitalRead(Btn_Small_FWD) == HIGH) count++;
+  if (digitalRead(Btn_Small_REV) == HIGH) count++;
+  if (digitalRead(Btn_Big_FWD)   == HIGH) count++;
+  if (digitalRead(Btn_Big_REV)   == HIGH) count++;
   return (count == 1);
 }
+static const float Distance  = 0.56;//il faut mesurer precisement apres
+float calcul_position(int pas){
+  return float(pas) * Distance ;
+}
+void print(){
+  Serial.print("counter_pas = ");
+  Serial.println(counter_pas);
+  position_act=calcul_position(counter_pas);
+  Serial.print("position_act = ");
+  Serial.println(position_act, 2); 
+}
 void button_control_stepper_motor(){
-  if (!only_one_button_pressed()) {
+  /*if (!only_one_button_pressed()) {
     stepper_stop();
     reset_position();
-    return;}
+    return;}*/
+  if(btn_pressed(Btn_Big_FWD)&btn_pressed(Btn_Big_REV)){
+    stepper_stop();
+    Serial.print("reinitialisation ;");
+    reset_position();
+    print();
+    return;
+  }  
   if(pressed_edge(Btn_Big_FWD)){
     stepper_motor_control_pas(STEP_FWD,PWM,vitesse,big_step);
-    counter_pas+=big_step;
     return;
   }else if(pressed_edge(Btn_Big_REV)){
     stepper_motor_control_pas(STEP_REV,PWM,vitesse,big_step);
-    counter_pas-=big_step;
     return;
   }else if(pressed_edge(Btn_Small_FWD)){
     stepper_motor_control_pas(STEP_FWD,PWM,vitesse,1);
-    counter_pas+=1;
     return;
     }else if(pressed_edge(Btn_Small_REV)){
     stepper_motor_control_pas(STEP_REV,PWM,vitesse,1);
-    counter_pas-=1;
     return;
   }else{
     stepper_stop();
@@ -218,7 +233,7 @@ void button_control_stepper_motor(){
 } 
 void button_control_servo_motor(){
   if(pressed_edge(Btn_Shutter)){
-    if(Shutter_Open & (is_limit_capShutter_pressed()==0)){
+    if(Shutter_Open){
       servo_close();
       Shutter_Open= 0;
       digitalWrite(Shutter_Led, HIGH);
@@ -229,11 +244,6 @@ void button_control_servo_motor(){
     }
   }
 } 
-
-static const float Distance  = 0.56;//il faut mesurer precisement apres
-float calcul_position(int pas,int position_act){
-  return float(pas) * Distance +float(position_act);
-}
 /* =========================================================
    LCD Screen
    =========================================================*/
@@ -272,7 +282,6 @@ void lcd_update_irma() {
   unsigned long now = millis();
   if (now - lcd_last_ms < LCD_PERIOD_MS) return;
   lcd_last_ms = now;
-
   bool limREV     = (digitalRead(cap_REV) == LOW);
   bool limFWD     = (digitalRead(cap_FWD) == LOW);
   bool shutClosed = (digitalRead(cap_Shutter) == LOW);
@@ -284,8 +293,7 @@ void lcd_update_irma() {
   l0 += "N:";
   l0 += String(counter_pas);
 
-
-  l1 += " CL:";
+  String l1 = "CL:";
   l1 += (shutClosed ? "1" : "0");
   l1 += " R";
   l1 += (limREV ? "1" : "0");
@@ -300,17 +308,22 @@ void lcd_update_irma() {
 /* =========================================================
    MAIN TEST 
    =========================================================*/
+int counter_last =0;
 void setup() {
+  Serial.begin(9600); 
+  while (!Serial) { }
   servo_init();
   stepper_init();
   cap_init();
   Init_Btn();
-  initialisation_position();
+  //initialisation_position();
   lcd_init_irma();
+  Serial.println("Hello ");
 }
 void loop() {
+  counter_last= counter_pas;
   button_control_stepper_motor();
-  position_act=calcul_position(counter_pas,position_act);
-  button_control_servo_motor(); 
-  lcd_update_irma();
+  
+  button_control_servo_motor();
+  //lcd_update_irma();
 }
